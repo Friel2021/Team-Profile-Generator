4dbacc168d67e3d6623b879c4f23ec3c48425c1a
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-08-24 17:00:49 -0600

    updated index intern

--- index.js
+++ index.js
@@ -1,70 +1,130 @@
-const Manager = require(".lib/Manager");
-const Engineer = require(".lib/Engineer");
-const Intern = require(".lib/Intern");
-const Employee = require(".lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
+const validator = require("");
 const fs = require("fs");
 
-const employees = [];
+// create generated html file path
+const generatedHtmlFilePath = "./dist/TeamRoster.html";
 
-function addEmployee(role) {
-  role = role.toLowerCase();
-  return inquirer
+let teamMembers = [];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is your managers name?",
+    },
+    {
+      type: "input",
+      name: "managerID",
+      message: "What is your managers ID #?",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is your managers email address?",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "What is your managers office number?",
+    },
+    {
+      type: "list",
+      name: "extraTeamMember",
+      message: "Please select a team member to add",
+      choices: ["Engineer", "Intern"],
+    },
+    {
+      type: "input",
+      message: "What is your github username?",
+      name: "Github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ])
+
+  .then((answers) => {
+    fs.writeFileSync(generatedHtmlFilePath, "");
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+    } else {
+    }
+  });
+
+// calls to have extra team member added - as many times as needed
+function addIntern() {
+  inquirer
     .prompt([
       {
-        name: "name",
-        message: `What is your ${role}'s name?`,
+        type: "input",
+        name: "internName",
+        message: "What is your interns name?",
       },
       {
-        name: "id",
-        message: `What is your ${role}'s id?`,
+        type: "input",
+        name: "internID",
+        message: "What is your interns ID #?",
       },
       {
-        name: "email",
-        message: `What is your ${role}'s email?`,
+        type: "input",
+        name: "internEmail",
+        message: "What is your managers email address?",
       },
       {
-        name: "officeNumber",
-        message: `What is your ${role}'s office number?`,
-        when: role === "manager",
+        type: "input",
+        name: "internSchool",
+        message: "What is your interns school?",
       },
       {
-        name: "github",
-        message: `What is your ${role}'s GitHub username?`,
-        when: role === "engineer",
-      },
-      {
-        name: "school",
-        message: `What is your ${role}'s school?`,
-        when: role === "intern",
+        type: "list",
+        name: "extraTeamMember",
+        message: "Please select a team member to add",
+        choices: ["Engineer", "Intern", "Exit"],
       },
     ])
-    .then((answers) => {
-      switch (role) {
-        case "manager":
-          return employees.push(
-            new Manager(
-              answers.name,
-              answers.id,
-              answers.email,
-              answers.officeNumber
-            )
-          );
-        case "engineer":
-          return employees.push(
-            new Engineer(
-              answers.name,
-              answers.id,
-              answers.email,
-              answers.github
-            )
-          );
-        case "intern":
-          return employees.push(
-            new Intern(answers.name, answers.id, answers.email, answers.school)
-          );
-        default:
-          return;
-      }
-    });
+
+    .then(
+      ((answers) => {
+        let intern = new Intern(
+          answers.internName,
+          answers.internID,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(intern);
+        if (answers.additionalTeamMember === "Engineer") {
+          addEngineer();
+        } else if (answers.additionalTeamMember === "Intern") {
+          addIntern();
+        } else {
+          // Finish and generate HTML
+          generateHTML();
+        }
+      }).catch((error) => {
+        if (error.isTtyError) {
+        } else {
+        }
+      })
+    );
 }
+// loop through array and generate HTML
+function generateHTML() {}
+
+// Function call to initialize app
+//       function init() {
+//         return inquirer.prompt(questions).then((data) => {
+//           writeToFile("GENERATEDREADME.md", generateMarkdown(data));
+//         });
+//       }
+
+//       init();
+//
