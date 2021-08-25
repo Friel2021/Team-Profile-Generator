5ac48e07d909c2d8a2304c1e8e6c9a6943b1bbbb
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-08-25 10:00:01 -0600

    updated push - 0825

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // create generated html file path
-const generatedHtmlFilePath = "./dist/TeamRoster.html";
+const generatedHtmlFilePath = "./dist/rosterListing.html";
+let teamMembers = [];
 
 inquirer
   .prompt([
@@ -31,27 +32,25 @@ inquirer
     },
     {
       type: "list",
-      name: "extraTeamMember",
+      name: "additionalTeamMember",
       message: "Please select a team member to add",
-      choices: ["Engineer", "Intern"],
+      choices: ["Engineer", "Intern", "Exit"]
     },
   ])
 
-  .then((answers) => {
-    let manager = new Manager(
-      answers.managerName,
-      answers.managerID,
-      answers.managerEmail,
-      answers.managerOfficeNumber
-    );
+  .then(answers => {
+    let manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
+
     teamMembers.push(manager);
-    evaluateExtraTeamMemberResult(answers.ExtraTeamMember);
+    evaluateAdditionalTeamMemberResult(answers.additionalTeamMember);
   })
-  .catch((error) => {
+  .catch(error => {
     if (error.isTtyError) {
     } else {
     }
   });
+
+
 
 function addEngineer() {
   inquirer
@@ -78,30 +77,23 @@ function addEngineer() {
       },
       {
         type: "list",
-        name: "extraTeamMember",
-        message: "Please select a team member to add",
+        name: "additionalTeamMember",
+        message: "Please select a team member to add or Exit to exit",
         choices: ["Engineer", "Intern", "Exit"],
       },
     ])
 
-    .then(
-      ((answers) => {
-        let engineer = new Engineer(
-          answers.engineerName,
-          answers.engineerID,
-          answers.engineerEmail,
-          answers.engineerGitHub
-        );
-        teamMembers.push(engineer);
-        evaluateExtraTeamMemberResult(answers.ExtraTeamMember);
-      }).catch((error) => {
-        if (error.isTtyError) {
-        } else {
-        }
-      })
-    );
+    .then(answers => {
+      let engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub);
+      teamMembers.push(engineer);
+      evaluateAdditionalTeamMemberResult(answers.additionalTeamMember);
+    })
+    .catch(error => {
+      if (error.isTtyError) {
+      } else {
+      }
+    });
 }
-
 // calls to have extra team member added - as many times as needed
 function addIntern() {
   inquirer
@@ -128,31 +120,25 @@ function addIntern() {
       },
       {
         type: "list",
-        name: "extraTeamMember",
-        message: "Please select a team member to add",
+        name: "additionalTeamMember",
+        message: "Please select a team member to add or Exit to exit",
         choices: ["Engineer", "Intern", "Exit"],
       },
     ])
 
-    .then(
-      ((answers) => {
-        let intern = new Intern(
-          answers.internName,
-          answers.internID,
-          answers.internEmail,
-          answers.internSchool
-        );
-        teamMembers.push(intern);
-        evaluateExtraTeamMemberResult(answers.ExtraTeamMember);
-      }).catch((error) => {
-        if (error.isTtyError) {
-        } else {
-        }
-      })
-    );
+    .then(answers => {
+      let intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+      teamMembers.push(intern);
+      evaluateAdditionalTeamMemberResult(answers.additionalTeamMember);
+    })
+    .catch(error => {
+      if (error.isTtyError) {
+      } else {
+      }
+    });
 }
 
-function evaluateExtraTeamMemberResult(result) {
+function evaluateAdditionalTeamMemberResult(result) {
   if (result === "Engineer") {
     addEngineer();
   } else if (result === "Intern") {
@@ -161,9 +147,61 @@ function evaluateExtraTeamMemberResult(result) {
     // Finish and generate HTML
     generateHTML();
   }
+}function generateInitialHTML()
+{
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="teamNavBar">
+            <h1 class="navBarTitle">My Team</h2>
+        </div>
+        <div class="cardBody">`
 }
 
-// loop through array and generate HTML
-function generateHTML() {
-  fs.writeFileSync(generatedHtmlFilePath, "");
+function generateTeamMemberHtml(teamMember)
+{
+    return `        <div class="teamMemberCard">
+    <div class="teamMemberTitle">
+        <h3>${teamMember.getName()} - ${teamMember.getRole()}</h3>
+    </div>
+    <div class="teamMemberBody">
+        <ul>
+            <li>ID:${teamMember.getId()}</li>
+            <li>Email: <a href="mailto:${teamMember.getEmail()}">${teamMember.getEmail()}</a></li>
+            ${teamMember.getRoleHtml()}
+        </ul>
+    </div>
+</div>`;
+}
+
+function generateFinalHtml()
+{
+    return `    </div>
+    </body>
+    </html>`;
+}
+
+//loop through team members array and generate html
+function generateHTML()
+{
+    //first create new file (overwrite existing if file exists)
+    fs.writeFileSync(generatedHtmlFilePath,"");
+    //setup string to hold generated html
+    let htmlData = generateInitialHTML();
+    //loop through team members
+    for(var a in teamMembers)
+    {
+        htmlData += generateTeamMemberHtml(teamMembers[a]);
+    }
+    //add final html to data
+    htmlData += generateFinalHtml();
+    //write data to file
+    fs.writeFileSync(generatedHtmlFilePath,htmlData);
 }
